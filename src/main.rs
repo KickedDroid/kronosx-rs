ca392b104a423c333578cf001ac1b2a1935628c2
Aidan <asmaidan@look.com>
2020-04-15 22:29:14 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ use std::sync::Arc;
 extern crate grpcio;
 
 pub mod protos;
-
+use protobuf::RepeatedField;
 use grpcio::{ChannelBuilder, EnvBuilder};
-use protos::replication_grpc::ReplicatorClient;
+use ::protos::replication_grpc::ReplicatorClient;
+use ::protos::replication::Replication;
 
 fn main() {
     let env = Arc::new(EnvBuilder::new().build());
     let ch = ChannelBuilder::new(env).connect("localhost:50051");
     let client = ReplicatorClient::new(ch);
 
-    let mut req = HelloRequest::default();
-    req.set_name("world".to_owned());
+    let mut req = Replication::default();
+    let bytes = "wbrbrb".as_bytes();
+    let v = RepeatedField::default();
+    v.to_vec();
+    req.set_cids_bytes(v: ::protobuf::RepeatedField<::std::vec::Vec<u8>>);
     let reply = client.say_hello(&req).expect("rpc");
     info!("Greeter received: {}", reply.get_message());
 }
