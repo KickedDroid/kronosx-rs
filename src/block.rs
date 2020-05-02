65528487f794dd81630de886cb614faa5be3cf06
Aidan <asmaidan@look.com>
2020-05-02 14:47:35 -0500

    revert

--- src/block.rs
+++ src/block.rs
@@ -1,9 +1,27 @@
-use ::protos::node{BlockstoreRequest};
+use ::protos::node::{BlockstoreRequest, DagRequest, BSREQTYPE};
 use ::protos::node_grpc::NodeApiClient;
+use std::sync::Arc;
+use grpcio::{ChannelBuilder, EnvBuilder};
+use protobuf::RepeatedField;
 
 pub fn new_node_client() -> NodeApiClient {
     let env = Arc::new(EnvBuilder::new().build());
-    let ch = ChannelBuilder::new(env).connect("localhost:50051");
+    let ch = ChannelBuilder::new(env).connect("xapi.temporal.cloud:9090");
     let client = NodeApiClient::new(ch);
     client
+}
+
+pub fn create_block_req() -> BlockstoreRequest {
+    let mut block_req = BlockstoreRequest::default();
+    let req_type = BSREQTYPE::BS_PUT;
+    block_req.set_requestType(req_type);
+
+    let mut data = Vec::new();
+    data.push("hey bruh".as_bytes().to_vec());
+    let v = RepeatedField::from_vec(data);
+    block_req.set_data(v);
+
+    block_req.set_cidVersion("v1".to_string());
+    block_req.set_hashFunc("sha2-256".to_string());
+    block_req
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use futures::{Future, Sink};
 
 extern crate cryptoxide;
 extern crate grpcio;
-
+use cid::{Cid, Codec, Version};
+use multihash::Sha2_256;
 pub mod protos;
 
 mod fileapi;
@@ -11,29 +12,24 @@ mod rep;
 use rep::{get_signature, new_rep_client};
 mod statusapi;
 use statusapi::{create_empty_req, new_status_client};
+mod block;
+use block::{new_node_client, create_block_req};
 
 fn main() {
-    let file_cli = new_file_client();
-    let rep_cli = new_rep_client();
-    let stat_cli = new_status_client();
+    let _file_cli = new_file_client();
+    let _rep_cli = new_rep_client();
+    let _stat_cli = new_status_client();
 
-    let req = create_empty_req();
-    let stat_resp = stat_cli.version(&req).expect("RPC Failed");
+    let block_cli = new_node_client();
 
-    let version = stat_resp.get_version();
-    println!("{:?}", version);
+    let block_req = create_block_req();
+    block_cli.blockstore(&block_req).expect("RPC Failed");
 
-    let (mut req, resp) = file_cli.upload_file().expect("RPC Failed");
+    println!("DOne?");
 
-    let stream = req.wait();
+    let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&data));
 
-    match resp.wait() {
-        Err(e) => panic!("{:?}", e),
-        Ok(item) => {
-            let hash = item.get_hash();
-            println!("{:?}", hash)
-        }
-    }
+
 }
 
 #[cfg(test)]
@@ -41,12 +37,19 @@ mod tests {
     use super::*;
     #[test]
     fn test_status_api() {
+
+        // Create a StatusAPiClient
         let stat_cli = new_status_client();
 
         let req = create_empty_req();
         let stat_resp = stat_cli.version(&req).expect("RPC Failed");
-
+        let stat_resp2 = stat_cli.status(&req).expect("RPC Failed");
         let version = stat_resp.get_version();
+        let stats = stat_resp2.get_status();
+
+        // Ex. ONLINE 
+        // "v3.4.0-1-ge94271e"
+        println!("{:?}", stats);
         println!("{:?}", version);
     }
 }
